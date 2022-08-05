ba44c9b4de8b2ff984131f68bef9f45e91ec1de0
Narenthar <narenthar31@gmail.com>
2022-08-05 23:36:50 +0530

    create Note

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,3 +1,10 @@
+const { getNotes, createNote } = require("../controllers/noteControllers");
+const { protect } = require("../middleware/authMiddleware");
+
 const router = require("express").Router();
+
+router.route("/").get(protect, getNotes);
+router.route("/create").post(protect, createNote);
+// router.route("/:id").get().put().delete();
 
 module.exports = router;
