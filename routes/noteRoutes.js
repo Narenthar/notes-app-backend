d071908e32adb13908ca7fcc41626e42f9e9e3d5
Narenthar <narenthar31@gmail.com>
2022-08-06 01:09:01 +0530

    delete notes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,10 +1,20 @@
-const { getNotes, createNote } = require("../controllers/noteControllers");
+const {
+  getNotes,
+  createNote,
+  getNoteById,
+  updateNote,
+  deleteNote,
+} = require("../controllers/noteControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = require("express").Router();
 
 router.route("/").get(protect, getNotes);
 router.route("/create").post(protect, createNote);
-// router.route("/:id").get().put().delete();
+router
+  .route("/:id")
+  .get(getNoteById)
+  .put(protect, updateNote)
+  .delete(protect, deleteNote);
 
 module.exports = router;
