5bbdd1007179a0a339cc44e52cbeaec427a4bf54
Narenthar <narenthar31@gmail.com>
2022-08-05 22:59:10 +0530

    note model

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const notes = require("./data/notes");
+// const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./connection");
 const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -17,11 +18,12 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
